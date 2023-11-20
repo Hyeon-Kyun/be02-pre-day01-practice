4c3f301871528fcce9eb74ff6f3cca0e1a15797f
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2023-11-20 16:05:17 +0900

    자바 문제

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -69,7 +69,7 @@ public class Ex03 {
                 for (Integer l = 1; l < 6 ; l = l +1) {
                     System.out.printf("*");
                 }
-
+                System.out.println();
 
             }
         }
@@ -91,32 +91,33 @@ public class Ex03 {
          ----------
          */
 
-        for (Integer i = 4 ; i <= 0 ; i = i-1) {
-            if (i > 0) {
-                System.out.printf(" ");
-            }else {
+        for (Integer i = 1 ; i < 6 ; i = i+1) {
+            if (i < 2) {
+                for (Integer j = 1 ; j < 5 ; j = j+1) {
+                    System.out.printf(" ");
+                }
                 System.out.println("*");
-            }
-
-        }
-
-        for (Integer i =1 ; i < 8 ; i = i + 1) {
-            if (i == 1) {
-                System.out.println("----------");
-            }else if(i < 3) {
-                System.out.println("    *");
-            }else if(i < 4) {
-                System.out.println("   ***");
-            }else if(i < 5) {
-                System.out.println("  *****");
-            }else if(i < 6) {
-                System.out.println(" *******");
-            }else if(i < 7) {
-                System.out.println("*********");
+            }else if (i < 3) {
+                for (Integer k = 1 ; k < 4 ; k = k+1) {
+                    System.out.printf(" ");
+                }
+                System.out.println("***");
+            }else if (i < 2) {
+                for (Integer l = 1 ; l < 3 ; l = l+1) {
+                    System.out.printf(" ");
+                }
+                System.out.println("*****");
+            }else if (i < 1) {
+                for (Integer m = 1 ; m < 2 ; m = m+1) {
+                    System.out.printf(" ");
+                }
+                System.out.println("*******");
             }else {
-                System.out.println("----------");
+                System.out.println("*********");
             }
         }
+
+
 
     }
 }
